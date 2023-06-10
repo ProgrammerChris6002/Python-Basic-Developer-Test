dbb8e28817cf6331bab90f4158d427d5fb00893f
Christian Nwachukwu <programmerchris6002@gmail.com>
2023-06-10 21:26:42 +0100

    Adding 7th question answer

---
+++ answers/seven.py
@@ -0,0 +1,17 @@
+# Funtion to recursively search for user's inputted number
+def recursive_search(numbers, target, start = 0):
+    if start >= len(numbers):
+        return False
+    if numbers[start] == target:
+        return True
+    return recursive_search(numbers, target, start + 1)
+
+numbers = [34, 67, 23, 54, 89, 14, 43]
+numbers.sort()
+target = int(input("Enter a number to search for: "))
+found = recursive_search(numbers, target)
+
+if found:
+    print("Number was found in the list.")
+else:
+    print("Number was not found in the list.")
